76dbdd7f9925d75e5ff4e7fcc09480b12d164b3f
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-09-02 18:01:01 +0000

    commit1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4bf0a31edd975ee59d6f53b738ed53f5283e3ac0a0e2f3832a19a6d9e288ddecb8abe62c2fddf0e9d25ab5dcccc1e2fcf82298abe0d3fe771906e60e85c0ef60'
