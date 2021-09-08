dc6819eea1e210b81043c165d92529c41ffb37ea
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-09-08 15:29:14 +0000

    commit5

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,11 +6,11 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image)  end
+  end
 
   def destroy
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+    #redirect_to post_image_path(params[:post_image_id])
   end
 
   private
